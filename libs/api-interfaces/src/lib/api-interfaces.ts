1b099428472334aa7c3f01f9b5558815527433a5
Ardit Daku <arditdaku1@gmail.com>
2021-10-23 11:40:20 +0200

    Init

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,41 @@
 export interface Message {
   message: string;
 }
+
+
+export interface Customer {
+  customerId: string;
+  customerName: string;
+  phone: string;
+  customerAddress: string;
+}
+
+export interface Order {
+  orderId: string;
+  orderNumber: number;
+  customerId: string;
+  orderDate: string;
+  totalAmount: number;
+  customerAddress: string;
+}
+
+export interface Supplier {
+  supplierId: string;
+  companyName: string;
+  phone: string;
+  address: string;
+}
+
+export interface Product {
+  productId: string;
+  productName: string;
+  supplierId: string;
+  unitPrice: number;
+}
+
+export interface OrderItem {
+  orderId: string;
+  productId: string;
+  unitPrice: number;
+  quantity: number;
+}
